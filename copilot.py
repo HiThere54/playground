7d49f06d6ecf2c476e6a48c564a713a6abecedc0
HiThere54 <96961737+HiThere54@users.noreply.github.com>
2022-06-10 00:35:20 +0000

    hi

---
+++ copilot.py
@@ -0,0 +1,78 @@
+# ask the user their name using input, store in varible name
+def getUserName():
+    name = input("What is your name?")
+    return name
+myname = getUserName()
+print("Hello " + myname)     # print the name
+# ask the user their age using input, store in varible age\
+def getUserAge():
+    age = input("How old are you?")
+    return age
+myage = getUserAge()
+print("You are " + myage + " years old")
+# ask the user their height using input, store in varible height
+def getUserHeight():
+    height = input("How tall are you?")
+    return height
+myheight = getUserHeight()
+print("You are " + myheight + " inches tall")
+# ask the user their weight using input, store in varible weight
+def getUserWeight():
+    weight = input("How much do you weigh?")
+    return weight
+myweight = getUserWeight()
+print("You weigh " + myweight + " pounds")
+# ask the user their shoe size using input, store in varible shoe
+def getUserShoeSize():
+    shoe = input("What is your shoe size?")
+    return shoe
+myshoe = getUserShoeSize()
+print("Your shoe size is " + myshoe)
+# ask the user their favorite color using input, store in varible color
+def getUserColor():
+    color = input("What is your favorite color?")
+    return color
+mycolor = getUserColor()
+print("Your favorite color is " + mycolor)
+# ask the user their favorite food using input, store in varible food
+def getUserFood():
+    food = input("What is your favorite food?")
+    return food
+myfood = getUserFood()
+print("Your favorite food is " + myfood)
+# ask the user their favorite movie using input, store in varible movie
+def getUserMovie():
+    movie = input("What is your favorite movie?")
+    return movie
+mymovie = getUserMovie()
+print("Your favorite movie is " + mymovie)
+# ask the user their favorite song using input, store in varible song
+def getUserSong():
+    song = input("What is your favorite song?")
+    return song
+mysong = getUserSong()
+print("Your favorite song is " + mysong)
+# ask the user their favorite sport using input, store in varible sport
+def getUserSport():
+    sport = input("What is your favorite sport?")
+    return sport
+mysport = getUserSport()
+print("Your favorite sport is " + mysport)
+# ask the user their favorite band using input, store in varible band
+def getUserBand():
+    band = input("What is your favorite band?")
+    return band
+myband = getUserBand()
+print("Your favorite band is " + myband)
+# ask the user their favorite book using input, store in varible book
+def getUserBook():
+    book = input("What is your favorite book?")
+    return book
+mybook = getUserBook()
+print("Your favorite book is " + mybook)
+# ask the user their favorite game using input, store in varible game
+def getUserGame():
+    game = input("What is your favorite game?")
+    return game
+mygame = getUserGame()
+print("Your favorite game is " + mygame)
